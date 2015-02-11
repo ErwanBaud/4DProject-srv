67a23c1a89110f631ffcac14bfea8486fecf0539
Denis Lebreil <Denis-Lebreil@users.noreply.github.com>
2015-02-11 15:27:47 +0100

    Lancement des clients via ssh

--- Serveur.cpp
+++ Serveur.cpp
@@ -1,4 +1,5 @@
 #include "Serveur.h"
+#include "ssh2_exec.h"
 
 #define timeOut 3
 
@@ -25,7 +26,7 @@ Serveur::Serveur()
 
     connect(signalMapper, SIGNAL(mapped(int)), this, SLOT(envoyerOrdre(int)));
 
-    comboBoxSelection->InsertAlphabetically;
+    //comboBoxSelection->InsertAlphabetically;
     comboBoxSelection->setMinimumWidth(110);
     comboBoxSelection->addItem("All");
     connect(comboBoxSelection , SIGNAL(currentIndexChanged(int)), this, SLOT(refreshButtons(int)));
@@ -252,8 +253,8 @@ void Serveur::deadCollector()
  *  */
 void Serveur::startClient()
 {    
-    QString program = QDir::home().filePath("4DProject/deploy/cltCore.exe");
-    QProcess *myProcess = new QProcess(this);
+    //QString program = QDir::home().filePath("4DProject/deploy/cltCore.exe");
+    /*QProcess *myProcess = new QProcess(this);
     if(myProcess->startDetached(program))
     {
         listeMessages->append("");
@@ -265,7 +266,21 @@ void Serveur::startClient()
         listeMessages->append("");
         listeMessages->append("Client non démarré !");
         listeMessages->append("");
+    }*/
+    if(!ssh_exec_command("127.0.0.1","projet4d","nohup 4DProject/deploy/cltCore.exe  > /dev/null 2> /dev/null </dev/null &"))
+    {
+        listeMessages->append("");
+        listeMessages->append("Client démarré !");
+        listeMessages->append("");
     }
+    else
+    {
+        listeMessages->append("");
+        listeMessages->append("Client non démarré !");
+        listeMessages->append("");
+    }
+
+
 }
 
 
--- srv.pro
+++ srv.pro
@@ -10,13 +10,24 @@ TARGET = srv
 TEMPLATE = app
 
 
+
 SOURCES += main.cpp \
     Serveur.cpp \
-    Client.cpp
+    Client.cpp \
+    ssh2_exec.cpp
 
 HEADERS  += \
     Serveur.h \
-    Client.h
+    Client.h \
+    libssh2_config.h \
+    ssh2_exec.h
 
 FORMS    += \
     Serveur.ui
+
+win32:CONFIG(release, debug|release): LIBS += -L$$PWD/../../../../../usr/local/lib/release/ -lssh2
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/../../../../../usr/local/lib/debug/ -lssh2
+else:unix: LIBS += -L$$PWD/../../../../../usr/local/lib/ -lssh2
+
+INCLUDEPATH += $$PWD/../../../../../usr/local/lib
+DEPENDPATH += $$PWD/../../../../../usr/local/lib
